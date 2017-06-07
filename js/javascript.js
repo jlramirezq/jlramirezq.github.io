e510d0c58d83befc1de71962dc8aacba74f59168
Pc <Pc@Pc-PC>
2017-06-07 17:20:23 -0500

    java sccript prueba 4564984

--- js/javascript.js
+++ js/javascript.js
@@ -17,6 +17,19 @@ $(document).ready(function(){
 								 });
 							});
 
+
+
+var x =	document.getElementById("like");
+x.onclick = megusta();
+		
+}
+
+function megusta(){
+	
+	alert("Gracias por darle me gusta");
+}
+
+
 windows.onload;
 
 
