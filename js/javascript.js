f37334091808e80f43995b45bcfbe3d8f7affed1
Pc <Pc@Pc-PC>
2017-06-07 17:08:53 -0500

    js

--- js/javascript.js
+++ js/javascript.js
@@ -19,15 +19,5 @@ $(document).ready(function(){
 
 
 
-var x =	document.getElementById("like");
-x.onclick = megusta();
-		
-}
-
-function megusta(){
-	
-	alert("Gracias por darle me gusta");
-}
-
 
 
