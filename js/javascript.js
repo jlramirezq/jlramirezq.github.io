c234b7033fcfb94f4d7074e31dec666ee0412025
Pc <Pc@Pc-PC>
2017-06-07 18:47:00 -0500

    js

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,20 @@
 
+$(document).ready(function(){
+	$('#biografia').click(function(){
+		$("#contenido").load("biografia.html");
+								 });
+	
+	$('#inicio').click(function(){
+		$("#contenido").load("inicio.html");
+								 });
 
+	$('#quehago').click(function(){
+		$("#contenido").load("quehago.html");
+								 });
 
+	$('#contacto').click(function(){
+		$("#contenido").load("contacto.html");
+								 });
+							});
+
+windows.onload;
